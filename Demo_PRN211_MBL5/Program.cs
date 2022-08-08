37b63efc12aeb5981bd35fbdf848372b89ce6c74
wood-crafter <phanhung2702@gmail.com>
2022-08-08 11:14:11 +0700

    Add project files.

---
+++ Demo_PRN211_MBL5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Demo_PRN211_MBL5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
